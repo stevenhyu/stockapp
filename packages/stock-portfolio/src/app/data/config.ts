25275787703b792a44ca09c830b6fdfe1d663925
hsiutayu <hsiutayu@gmail.com>
2022-02-27 15:58:45 -0800

    push the rest of app files

---
+++ packages/stock-portfolio/src/app/data/config.ts
@@ -0,0 +1,7 @@
+import * as core from 'stock-core';
+
+export class Config {
+    public static readonly NAME: string = "Ergasia";
+    public static readonly VERSION: string = core.Version;
+    public static readonly STGKEY: string = "wj-stockportfolio-items-v2";
+}
